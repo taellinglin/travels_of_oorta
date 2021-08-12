a30baf457492254b94cc2b652a88fcdd53829519
taellinglin <taellinglin@gmail.com>
2021-08-12 15:45:07 -0500

    added grasslands level

---
+++ tilesets/grass_tileset.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="grass_tileset" tilewidth="16" tileheight="16" tilecount="18" columns="3">
+ <image source="grass_tileset.png" width="48" height="96"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="Grasslands" color="#00ffff" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="3" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="4" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="5" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="6" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="7" wangid="1,1,0,0,0,0,0,1"/>
+   <wangtile tileid="8" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="9" wangid="1,1,0,0,0,1,1,1"/>
+   <wangtile tileid="10" wangid="1,1,0,0,0,0,0,1"/>
+   <wangtile tileid="11" wangid="1,1,1,1,0,0,0,1"/>
+   <wangtile tileid="12" wangid="0,0,0,0,0,1,1,1"/>
+   <wangtile tileid="14" wangid="0,1,1,1,0,0,0,0"/>
+   <wangtile tileid="15" wangid="0,0,0,1,1,1,1,1"/>
+   <wangtile tileid="16" wangid="0,0,0,1,1,1,0,0"/>
+   <wangtile tileid="17" wangid="0,1,1,1,1,1,0,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
